3bb2213afe8f14f4a5a9bcd3dd25b6a9436d099a
Kumar Bibek <coomar.101@gmail.com>
2016-02-20 04:13:16 +0530

    R

--- multipicker/src/main/java/com/kbeanie/multipicker/api/PickerManager.java
+++ multipicker/src/main/java/com/kbeanie/multipicker/api/PickerManager.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v4.app.Fragment;
 
 import com.kbeanie.multipicker.utils.FileUtils;
