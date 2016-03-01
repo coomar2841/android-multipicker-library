ef84e83a77edaf489178174b0022fb637e06db25
Kumar Bibek <coomar.101@gmail.com>
2016-03-02 02:20:59 +0530

    Disable CR in debug

--- app/src/main/java/com/kbeanie/multipicker/sample/AbApplication.java
+++ app/src/main/java/com/kbeanie/multipicker/sample/AbApplication.java
@@ -1,6 +1,7 @@
 package com.kbeanie.multipicker.sample;
 
 import android.app.Application;
+
 import com.crashlytics.android.Crashlytics;
 import com.facebook.drawee.backends.pipeline.Fresco;
 
@@ -9,12 +10,14 @@ import io.fabric.sdk.android.Fabric;
 /**
  * Created by kbibek on 2/18/16.
  */
-public class AbApplication extends Application{
+public class AbApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, new Crashlytics());
+        }
         Fresco.initialize(this);
     }
 }
