b4fe5b2a110cbab8a8f1e94ec7a8475e0f1d69ad
Kumar Bibek <coomar.101@gmail.com>
2016-12-16 12:35:53 +0530

    Custom File Provider

---
+++ multipicker/src/main/java/com/kbeanie/multipicker/utils/AMPFileProvider.java
@@ -0,0 +1,10 @@
+package com.kbeanie.multipicker.utils;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by kbibek on 12/16/16.
+ */
+
+public class AMPFileProvider extends FileProvider {
+}
